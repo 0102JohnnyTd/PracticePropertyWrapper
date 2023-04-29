12ee8ac35317a8a6f910898ffe2ddd6751d1df0a
0102johnny <rt2824.recruit.1022@gmail.com>
2023-04-29 10:32:58 +0900

    Initial commit

---
+++ PracticePropertyWrapper/PracticePropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticePropertyWrapperApp.swift
+//  PracticePropertyWrapper
+//
+//  Created by Johnny Toda on 2023/04/29.
+//
+
+import SwiftUI
+
+@main
+struct PracticePropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
