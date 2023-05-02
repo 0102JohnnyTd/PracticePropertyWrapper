8d24b4282701cc675396fdce3d0e2ad95567d048
0102johnny <rt2824.recruit.1022@gmail.com>
2023-05-02 10:09:43 +0900

    @StateObjectと@ObservedObjectの違いについて理解する為のファイルを作成

---
+++ PracticePropertyWrapper/ReferenceType/SwitchColorView.swift
@@ -0,0 +1,20 @@
+//
+//  SwitchColorView.swift
+//  PracticePropertyWrapper
+//
+//  Created by Johnny Toda on 2023/05/02.
+//
+
+import SwiftUI
+
+struct SwitchColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwitchColorView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwitchColorView()
+    }
+}
