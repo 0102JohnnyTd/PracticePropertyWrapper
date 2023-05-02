ac8de9e6a9288f50365a0cf08415e51e4c0fb72c
0102johnny <rt2824.recruit.1022@gmail.com>
2023-05-03 00:06:41 +0900

    親ViewのChange colorボタンをタップするとStateObject countは値が変化しないが、ObservedObject countは値が0に変化してしまう理由をコメントアウトで追記

--- PracticePropertyWrapper/ReferenceType/SwitchColorView.swift
+++ PracticePropertyWrapper/ReferenceType/SwitchColorView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 // ボタンをタップするとサークルのカラーが切り替わるViewを定義
 struct SwitchColorView: View {
+    // 値型のデータをView自身に保持させるnode@Stateを使用
     @State private var isDanger = false
 
     var body: some View {
         VStack {
             Spacer()
+            // true = 赤色, false = 緑色
             isDanger == true ? Circle().foregroundColor(.red)
                 .frame(width: 200, height: 200) :
             Circle().foregroundColor(.green)
                 .frame(width: 200, height: 200)
+
+            // 処理を実行するとデータに変更が加えられる為,bodyが再描画される
+            // bodyが再描画されると@ObservedObjectのライフサイクルが呼ばれる
             Button("Change color") {
+                // isDangerのBool値を反転させる
                 isDanger.toggle()
             }
             Spacer()
@@ -40,12 +46,16 @@ struct StateObjectCountView: View {
             Button("increment") {
                 dataSource.count += 1
             }
+//            ObservedObjectCountView(dataSource: dataSource)
         }
     }
 }
 
 // @ObservedObjectを付与したデータオブジェクトを持つ構造体を定義
 struct ObservedObjectCountView: View {
+    // 本来ObservedObjectは親Viewから渡されるデータを参照する場合のみ用いる
+    //    @ObservedObject var dataSource: DataSource
+    //自身でデータオブジェクトを保持すべきではない。
     @ObservedObject private var dataSource = DataSource()
 
     var body: some View {
